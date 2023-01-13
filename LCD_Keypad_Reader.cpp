2876fc285ad00aac3e4998964e7b7cff79192419
Haoyuan Kevin Xia <kevin062397@outlook.com>
2023-01-13 14:18:17 -0800

    Add .gitignore and update code format

--- LCD_Keypad_Reader.cpp
+++ LCD_Keypad_Reader.cpp
@@ -18,89 +18,97 @@ static int DEFAULT_KEY_PIN = 0;
 #define _display_custom_
 
 #ifdef _display_standard_
-  // The Sainsmart keypad uses a voltage divider to deliver a voltage
-  // between 0 and 5 V that corresponds to the key being pressed in
-  // order to use only a single input pin. The values below are from 0 to
-  // 1023 because the Arduino uses a 10 bit resolution.
-  /* Board introduced by G.W.   */
-  static int DEFAULT_THRESHOLD = 50;   // KH: changed from 5 to 50 - more tolerance
-  static int UPKEY_ARV = 144; // 0.720 V, that's read "analogue read value"
-  static int DOWNKEY_ARV = 329; // 1.645 V
-  static int LEFTKEY_ARV = 505; // 2.525 V
-  static int RIGHTKEY_ARV = 0; // 0 V
-  static int SELKEY_ARV = 742; // 3.710 V
-  static int NOKEY_ARV = 1023; // 5.115 V
+// The Sainsmart keypad uses a voltage divider to deliver a voltage
+// between 0 and 5 V that corresponds to the key being pressed in
+// order to use only a single input pin. The values below are from 0 to
+// 1023 because the Arduino uses a 10 bit resolution.
+/* Board introduced by G.W.   */
+static int DEFAULT_THRESHOLD = 50; // KH: changed from 5 to 50 - more tolerance
+static int UPKEY_ARV = 144;		   // 0.720 V, that's read "analogue read value"
+static int DOWNKEY_ARV = 329;	   // 1.645 V
+static int LEFTKEY_ARV = 505;	   // 2.525 V
+static int RIGHTKEY_ARV = 0;	   // 0 V
+static int SELKEY_ARV = 742;	   // 3.710 V
+static int NOKEY_ARV = 1023;	   // 5.115 V
 #endif
 
 #ifdef _display_alternative1_
-  // K.H.: My NEW Board (looks same as that one shown in project, but branded as: Open Source ELECTRONICS - Miroad
-  //       has other voltage steps - here the values:  
-  //  IF ONLY THE RIGHT-KEY IS RECOGNIZED ON YOUR BOARD,  TRY THIS VALUES 
-  static int DEFAULT_THRESHOLD = 50;
-  static int UPKEY_ARV = 99;
-  static int DOWNKEY_ARV = 256;
-  static int LEFTKEY_ARV = 410;
-  static int RIGHTKEY_ARV = 0;
-  static int SELKEY_ARV = 640;
-  static int NOKEY_ARV = 1023;
+// K.H.: My NEW Board (looks same as that one shown in project, but branded as: Open Source ELECTRONICS - Miroad
+//       has other voltage steps - here the values:
+//  IF ONLY THE RIGHT-KEY IS RECOGNIZED ON YOUR BOARD,  TRY THIS VALUES
+static int DEFAULT_THRESHOLD = 50;
+static int UPKEY_ARV = 99;
+static int DOWNKEY_ARV = 256;
+static int LEFTKEY_ARV = 410;
+static int RIGHTKEY_ARV = 0;
+static int SELKEY_ARV = 640;
+static int NOKEY_ARV = 1023;
 #endif
 
 #ifdef _display_custom_
-  static int DEFAULT_THRESHOLD = 50;
-  static int UPKEY_ARV = 206;
-  static int DOWNKEY_ARV = 409;
-  static int LEFTKEY_ARV = 625;
-  static int RIGHTKEY_ARV = 0;
-  static int SELKEY_ARV = 824;
-  static int NOKEY_ARV = 1023;
+static int DEFAULT_THRESHOLD = 50;
+static int UPKEY_ARV = 206;
+static int DOWNKEY_ARV = 409;
+static int LEFTKEY_ARV = 625;
+static int RIGHTKEY_ARV = 0;
+static int SELKEY_ARV = 824;
+static int NOKEY_ARV = 1023;
 #endif
 
 LCD_Keypad_Reader::LCD_Keypad_Reader()
 {
-  _keyPin = DEFAULT_KEY_PIN;
-  _threshold = DEFAULT_THRESHOLD;
-  _curInput = NO_KEY;
-  _curKey = NO_KEY;
-  RepeatRate = keyRepeatRateSlow;
+	_keyPin = DEFAULT_KEY_PIN;
+	_threshold = DEFAULT_THRESHOLD;
+	_curInput = NO_KEY;
+	_curKey = NO_KEY;
+	RepeatRate = keyRepeatRateSlow;
 }
 
 int LCD_Keypad_Reader::ActRepeatRate()
 {
-  RepeatRate -= keyRepeatRateStep;
-  if (RepeatRate < keyRepeatRateHigh){
-    RepeatRate =keyRepeatRateHigh;
-  }
-  return RepeatRate;
+	RepeatRate -= keyRepeatRateStep;
+	if (RepeatRate < keyRepeatRateHigh)
+	{
+		RepeatRate = keyRepeatRateHigh;
+	}
+	return RepeatRate;
 }
 
 int LCD_Keypad_Reader::getKey()
 {
-  _curInput =  analogRead(_keyPin);
-  _curKey = categorizeKey(_curInput);
-  return _curKey;
+	_curInput = analogRead(_keyPin);
+	_curKey = categorizeKey(_curInput);
+	return _curKey;
 }
 
-int LCD_Keypad_Reader::categorizeKey(int analogKeyValue){
-  int categorizedKeyValue = 0;
+int LCD_Keypad_Reader::categorizeKey(int analogKeyValue)
+{
+	int categorizedKeyValue = 0;
 
-  if (analogKeyValue > UPKEY_ARV - _threshold && analogKeyValue < UPKEY_ARV + _threshold ){
-      categorizedKeyValue = UP_KEY;
-  }
-  else if (analogKeyValue > DOWNKEY_ARV - _threshold && analogKeyValue < DOWNKEY_ARV + _threshold ){
-      categorizedKeyValue = DOWN_KEY;
-  }
-  else if (analogKeyValue > RIGHTKEY_ARV - _threshold && analogKeyValue < RIGHTKEY_ARV + _threshold ){
-      categorizedKeyValue = RIGHT_KEY;
-  }
-  else if (analogKeyValue > LEFTKEY_ARV - _threshold && analogKeyValue < LEFTKEY_ARV + _threshold ){
-      categorizedKeyValue = LEFT_KEY;
-  }
-  else if (analogKeyValue > SELKEY_ARV - _threshold && analogKeyValue < SELKEY_ARV + _threshold ){
-      categorizedKeyValue = SELECT_KEY;
-  }
-  else{
-    categorizedKeyValue = NO_KEY;
-  }
+	if (analogKeyValue > UPKEY_ARV - _threshold && analogKeyValue < UPKEY_ARV + _threshold)
+	{
+		categorizedKeyValue = UP_KEY;
+	}
+	else if (analogKeyValue > DOWNKEY_ARV - _threshold && analogKeyValue < DOWNKEY_ARV + _threshold)
+	{
+		categorizedKeyValue = DOWN_KEY;
+	}
+	else if (analogKeyValue > RIGHTKEY_ARV - _threshold && analogKeyValue < RIGHTKEY_ARV + _threshold)
+	{
+		categorizedKeyValue = RIGHT_KEY;
+	}
+	else if (analogKeyValue > LEFTKEY_ARV - _threshold && analogKeyValue < LEFTKEY_ARV + _threshold)
+	{
+		categorizedKeyValue = LEFT_KEY;
+	}
+	else if (analogKeyValue > SELKEY_ARV - _threshold && analogKeyValue < SELKEY_ARV + _threshold)
+	{
+		categorizedKeyValue = SELECT_KEY;
+	}
+	else
+	{
+		categorizedKeyValue = NO_KEY;
+	}
 
-  return categorizedKeyValue;
+	return categorizedKeyValue;
 }
--- LCD_Keypad_Reader.h
+++ LCD_Keypad_Reader.h
@@ -23,26 +23,26 @@
 
 class LCD_Keypad_Reader
 {
-  public:
-    LCD_Keypad_Reader();
-    int getKey();
-    int categorizeKey(int);
-    int RepeatRate;
-    int ActRepeatRate();
-  private:
-    int _keyPin;
-    int _threshold;
-    int _curInput;
-    int _curKey;
+public:
+	LCD_Keypad_Reader();
+	int getKey();
+	int categorizeKey(int);
+	int RepeatRate;
+	int ActRepeatRate();
+
+private:
+	int _keyPin;
+	int _threshold;
+	int _curInput;
+	int _curKey;
 };
 
-const  int keySampleRate          = 100;      // ms between checking keypad for key
+const int keySampleRate = 100; // ms between checking keypad for key
 
 /* K.H: "Added KeyRepeatRateAcceleration" to   customize dynamic behavior */
-const  int keyRepeatRateHighDelay = 500;      // delay for speeding up
-const  int keyRepeatRateStep      =  60;      // speeding up steps
-const  int keyRepeatRateSlow      = 500;      // SLOW speed when held, key repeats 1000 / keyRepeatRate times per second
-const  int keyRepeatRateHigh      = 100;      // HIGH speed when held, key repeats 1000 / keyRepeatRate times per second
-
+const int keyRepeatRateHighDelay = 500; // delay for speeding up
+const int keyRepeatRateStep = 60;		// speeding up steps
+const int keyRepeatRateSlow = 500;		// SLOW speed when held, key repeats 1000 / keyRepeatRate times per second
+const int keyRepeatRateHigh = 100;		// HIGH speed when held, key repeats 1000 / keyRepeatRate times per second
 
 #endif
